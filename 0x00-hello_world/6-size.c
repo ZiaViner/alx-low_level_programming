0198e6b6b75194c4e1c8bf5fe50d5e0cc3f0b52c
ZiaViner <VinerZia2017@gmail.com>
2022-09-08 15:33:07 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main -  prints the size of various types on the computer it is compiled and run on.
+* main -  prints the size of various types on the computer 
 *
 * Description: using the main function
 * this program print various types of texts
