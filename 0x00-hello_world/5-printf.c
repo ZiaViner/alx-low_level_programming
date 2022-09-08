4d852bacd84348d6580cde0d279293bb580346c9
ZiaViner <VinerZia2017@gmail.com>
2022-09-08 15:08:10 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
- /**
-* main -  prints exactly with proper grammar, but the outcome is a piece of art,
-*
-* return: 0
-*/
-	int main(void)
-{ printf (" prints exactly with proper grammar, but the outcome is a piece of art,");
 
-return (0);
+/**
+* main - prints the string in the function
+*
+* Description: using the main function
+* this program prints with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
