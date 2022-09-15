66c52190e8e2323a272d7502d7a7ba9c131120c0
ZiaViner <111008446+ZiaViner@users.noreply.github.com>
2022-09-15 20:34:31 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,14 @@
  * main - tests function that prints if int is + or -
  * Return 0
  */
+
 int main(void)
+    
 {
     int i;
 
     i = 0; 
     positive_or_negative(1);
+    
     return (0);
 }
